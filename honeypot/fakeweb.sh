797159a7ba67ee704fd5e7329c82311a8c216c8f
Jonathan-R-Anderson <77250765+Jonathan-R-Anderson@users.noreply.github.com>
2025-09-01 11:40:56 -0700

    Replace honeyd with portspoof

--- honeypot/fakeweb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Simple fake HTTP response for honeypot
-# Each connection gets its own environment with port information.
-read request
-PORT="${HONEYD_LOCAL_PORT:-$HONEYD_PORT}"
-MESSAGE="Hello from port ${PORT}"
-printf 'HTTP/1.1 200 OK\r\nContent-Length: %d\r\n\r\n%s' "${#MESSAGE}" "$MESSAGE"
